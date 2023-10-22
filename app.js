7ed375025f6667157a75b5874f8ca439115c5277
Kikkeri-UI <sagarbharadwaj98@gmail.com>
2023-10-22 18:52:13 +0100

    About Us Section Functionality

--- app.js
+++ app.js
@@ -254,3 +254,32 @@ questions.forEach((question)=>{
        question.classList.toggle('show-text');
     })
 })
+
+// About Us Section
+
+const about = document.querySelector('.aboutcontainer');
+const btns = document.querySelectorAll('.tab-btn');
+const articles = document.querySelectorAll('.content');
+
+about.addEventListener('click',(e)=>{
+    const id = e.target.dataset.id;
+    if(id){
+        btns.forEach((btn)=>{
+            btn.classList.remove('active');
+        })
+        e.target.classList.add('active');
+        articles.forEach((article)=>{
+            article.classList.remove('active')
+        });
+        const element = document.getElementById(id);
+        element.classList.add('active');
+    }
+})
+
+
+
+
+// Footer Date
+
+const date = document.getElementById('date');
+date.innerHTML = new Date().getFullYear();
